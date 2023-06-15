ff1cdb82d56779b02b495e9f503b39c8ed73f91c
Jorge F. Sánchez (George) <jorge.sanchez@syrocon.de>
2023-06-15 22:32:36 +0200

    Add initial test setup

---
+++ tests/test_converter.py
@@ -0,0 +1,22 @@
+import yaml
+from props2yaml.converter import convert_properties_to_yaml
+
+
+def test_converter():
+    properties = """
+    config.props.value=50
+    config.key.value=150
+    config.key2.value=1000
+    key=0
+    key2=1
+    config.key3.value.value2="abc"
+    """
+    converted = convert_properties_to_yaml(properties)
+    yml = yaml.load(converted)
+    assert yml is not None
+    assert yml["config"]["props"]["value"] == "50"
+    assert yml["config"]["key"]["value"] == "150"
+    assert yml["config"]["key2"]["value"] == "1000"
+    assert yml["key"] == "0"
+    assert yml["key2"] == "1"
+    assert yml["config"]["key3"]["value"]["value2"] == '"abc"'
